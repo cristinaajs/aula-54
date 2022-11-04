f17d6e57582549ee37cf33299b53c190ccf1849d
cristinaaajs@gmail.com <cristinaaajs@gmail.com>
2022-11-03 21:02:43 -0300

    aula_54_PropriedadesComponentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
+//Importando um componente externo
+import HelloWorldDIO from './components/HelloWorldDIO';
+import HelloWorldDIOClass from './components/HelloWorldDIOClass';
+import HelloPeople from './components/HelloPeople';
+import Calculator from './components/Calculator';
+import HelloPerson from './components/HelloPerson';
+import HelloCalc from './components/HelloCalc';
+import HelloBaseClass from './components/HelloBaseClass';
+
+//Função que agrupa todos os componentes que serão renderizados
 function App() {
+
+  let a = 44
+  let b = 99
+
+  let person = {
+    name: 'Cristina',
+    surName: 'Jesus'
+  }
+
+  //Retorno da função
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* Chamando o componente importado */}
+      <HelloWorldDIO />
+      <HelloWorldDIOClass />
+      <HelloPeople name="Cristina" age={27}/>
+      <Calculator n1={a} n2={b}/>
+      <HelloPerson person={person}/>
+      <HelloCalc />
+      <HelloBaseClass />
+
     </div>
   );
 }
 
+//Exportar o componente App para ser renderizado
 export default App;
